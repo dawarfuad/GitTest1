866efaa6c846aa7e6371ebc03c48f06385e68a24
dawarfuad <35294494+dawarfuad@users.noreply.github.com>
2019-05-20 15:03:45 +0300

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //code was edited from GitHub
+            //code was edited again from GitHub
         }
     }
 }
